14a294a7ed13b2c4607d6017989f9f28b3cd89bb
sharveersingh96 <sharveersingh96@gmail.com>
2025-04-04 00:27:57 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("contactForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    alert("Thank you for reaching out! I'll get back to you soon.");
+});
